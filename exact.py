a9e9fed23a4ad083645131a1530705f0dd888529
Zeyad <35004403+ZeyadAl@users.noreply.github.com>
2025-02-13 21:43:36 +0300

    Update exact.py for zip

--- exact.py
+++ exact.py
@@ -1,14 +1,15 @@
 from funcs import *
+import pysam
+import io
 
-grch_path = "/Users/zeyad/Documents/school_stuff/kaust/249/Assignments/1/ncbi_dataset/ncbi_dataset/data/GCA_000001405.29/GCA_000001405.29_GRCh38.p14_genomic.fna"
-t2t_path = "/Users/zeyad/Documents/school_stuff/kaust/249/Assignments/1/ncbi_dataset/ncbi_dataset/data/GCA_009914755.4/GCA_009914755.4_T2T-CHM13v2.0_genomic.fna"
+grch_path = "/Users/zeyad/Documents/school_stuff/kaust/249/Assignments/1/ncbi_dataset/ncbi_dataset/data/GCA_000001405.29/GCA_000001405.29_GRCh38.p14_genomic.fna.gz"
+t2t_path = "/Users/zeyad/Documents/school_stuff/kaust/249/Assignments/1/ncbi_dataset/ncbi_dataset/data/GCA_009914755.4/GCA_009914755.4_T2T-CHM13v2.0_genomic.fna.gz"
 
 filename = t2t_path
 
 matches = 0
 
 def process_group(header, lines):
-    """Define your processing logic here"""
     global matches
     n = len(lines)
     for i in range(0, n - m + 1):
@@ -16,27 +17,22 @@ def process_group(header, lines):
             matches += 1
             print(f"Found match! {header}, start = {i}, end = {i+m-1}")
     
-#    print("Processing:", header)  # Example operation
-#    print("Number of lines:", len(lines))  # Example operation
-
 def read_groups_streaming(filename):
-    with open(filename, 'r') as file:
-        header = None
-        lines = ""
-
-        for line in file:
-            line = line.rstrip().upper()
-
-            if line.startswith(">"):  # New header found
-                if header is not None:
-                    process_group(header, lines)  # Process previous group
-                header = line  # Start a new group
-                lines = ""  # Reset the line list
-            else:
-                lines += line
-
-        if header is not None:
-            process_group(header, lines)  # Process the last group
+    with pysam.BGZFile(filename, 'r') as bgzf_file:
+        with io.TextIOWrapper(bgzf_file) as file:
+            header = None
+            lines = ""
+            for line in file:
+                line = line.rstrip().upper()
+                if line.startswith(">"):  # header
+                    if header is not None:
+                        process_group(header, lines)  # process
+                    header = line  # new group
+                    lines = ""  # reset
+                else:
+                    lines += line
+            if header is not None:
+                process_group(header, lines)  # last group
 
 read_groups_streaming(filename)
 print(f"num of matches={matches}")
